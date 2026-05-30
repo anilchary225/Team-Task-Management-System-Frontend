09282b14dcf00b34c85061104cb0f8638f5ad7ba
anilchary225 <vadlaanilchary225@gmail.com>
2026-05-30 13:12:45 +0530

    added backend original

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || '/api',
+  baseURL: import.meta.env.VITE_API_URL || 'https://team-task-management-system-backend.onrender.com/api',
 })
 
 API.interceptors.request.use(config => {
